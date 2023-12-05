6de7312acc12ca9f26663b398a61d91db3341f78
brayansuamon <brayansuamon@gmail.com>
2023-12-05 09:52:29 -0500

    Fixed tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,11 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
 import type { Config } from "tailwindcss";
 
 const config: Config = {
