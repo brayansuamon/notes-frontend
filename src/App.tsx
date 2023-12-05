421c7ffe6f0985fb47fe56045e054f0fb074a059
brayansuamon <brayansuamon@gmail.com>
2023-12-04 20:37:55 -0500

    Create the button to add Note

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 
 import './App.css';
-import Card from './components/common/Card';
+
+const openForm = ()=>{
+
+}
 
 function App() {
   return (
-    <div className="App w-full bg-text">
-     <Card />
+    <div className="App w-full min-h-screen h-full flex items-center justify-center">
+    <button className='p-4 bg-gray-700 text-white' onClick={openForm}>Create Note</button>
+    {/* <Card /> */}
     </div>
   );
 }
