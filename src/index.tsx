810c6387c27c5ae5971b19494d01b9c55746ef7e
brayansuamon <brayansuamon@gmail.com>
2023-12-06 21:04:34 -0500

    fixed tailwind issues

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'tailwindcss/tailwind.css';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
